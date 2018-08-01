ab96fc5ae3dd54f7c4c6f131e045bdb89124103d
Thidaporn Kijkamjai <jinglejill@me.com>
2018-08-01 11:37:43 +0700

    review url path to use only one place

---
+++ AndJummum Url/test.php
@@ -0,0 +1,13 @@
+1. db มี 3 อันหลัก
+    1. สำหรับแอพฝั่งลูกค้า (dbName: and_jummum)
+    2. สำหรับแอพฝั่งร้านอาหาร (dbName: and_jummum_om)
+    3. เมนูและตัว setup ของแต่ละร้าน (dbName: and_jummum_om_mamarin7,and_jummum_om_mamarin7_2,and_jummum_om_mamarin7_3,and_jummum_om_mamarin7_4
+  2. path วาง php ฝั่ง server แบ่งเป็น 2 อันหลัก
+  3. db username:andAdmin, password:111111
+  4. เข้า plesk username:android@jummum.co, password:Android1@
+  5. ftp username: androidFtp, password:Android1@
+
+
+
+
+
---
+++ AndJummum Url/testpush.php
@@ -0,0 +1,24 @@
+<?php
+    include_once('dbConnect.php');
+    setConnectionValue("AND_JUMMUM");
+    $arrBody = array(
+                     'alert' => 'test'//ข้อความ
+                      ,'sound' => 'default'//,//เสียงแจ้งเตือน
+//                      ,'badge' => 3 //ขึ้นข้อความตัวเลขที่ไม่ได้อ่าน
+                     ,'category' => 'Print'
+                      );
+    sendTestApplePushNotification('5286f1867af1269a190ffba95b265cf5a806e35ef9cdd800f980423c581ea5db',$arrBody);
+    
+//    sleep(5);
+//
+//
+//    $arrBody = array(
+//                     'alert' => 'เทสจิ๋ว 2
+//                     เอ'//ข้อความ
+//                     ,'sound' => 'default'//,//เสียงแจ้งเตือน
+//                     //                      ,'badge' => 3 //ขึ้นข้อความตัวเลขที่ไม่ได้อ่าน
+//                     );
+//    sendTestApplePushNotification('1877301d04f677b7fcc415b7f0bcbd799bf679013b14f76ce746d778087a22f6',$arrBody);
+?>
+
+//<table><tr><td style="text-align: center;border: 1px solid black; padding-left: 10px;padding-right: 10px; border-radius: 15px;">x</td></tr></table>
